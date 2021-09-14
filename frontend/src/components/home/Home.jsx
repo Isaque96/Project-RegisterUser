74810488cf4a4373ac2753f66f0210f589a7f61a
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-14 20:29:14 -0300

    Only Commit / Full Project

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from '../template/Main';
+
+export default props =>
+  <Main icon="home" title="Início" subtitle="Segundo Projeto em ReactJS">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Projeto feito para entender a construção de um cadastro feito em ReactJS</p>
+  </Main>
